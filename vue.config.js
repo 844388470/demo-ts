a64236f6ca9c9ad61c3ecc578079f7d815995302
liuqing <844388470@qq.com>
2020-09-04 16:57:06 +0800

    添加title

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  pages: {
+    index: {
+      entry: 'src/main.ts',        //page的主入口
+      template: 'public/index.html',    //模板来源
+      filename: 'index.html',            //在 dist/index.html 的输出
+      title: '多多之家',        
+      chunks: ['chunk-vendors', 'chunk-common', 'index'] 
+    }
+  },
+  productionSourceMap: false,
   devServer: {
     port: 3500, // 端口
   },
